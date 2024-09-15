c2fef0f9c5561aa7a195ec616893a0cc77fbf773
Nancy Singleton <nancysingleton_@hotmail.co.uk>
2024-09-15 16:24:26 +0100

    Add response codes

--- src/main/java/com/example/demo/web/CoffeeController.java
+++ src/main/java/com/example/demo/web/CoffeeController.java
@@ -4,6 +4,8 @@ import com.example.demo.domain.Coffee;
 import com.example.demo.dto.CoffeeDto;
 import com.example.demo.repository.CoffeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,18 +16,20 @@ public class CoffeeController {
     private CoffeeRepository coffeeRepository;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     Iterable<Coffee> getCoffees() {
         return coffeeRepository.findAll();
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     void postCoffee(@RequestBody CoffeeDto coffeeDto) {
         Coffee coffee = new Coffee(coffeeDto.name());
         coffeeRepository.save(coffee);
     }
 
     @PutMapping("/{id}")
-    void putCoffee(
+    ResponseEntity<Object> putCoffee(
             @PathVariable String id,
             @RequestBody CoffeeDto coffeeDto
     ) {
@@ -33,12 +37,15 @@ public class CoffeeController {
 
         if (coffeeRepository.existsById(id)) {
             coffeeRepository.updateNameById(coffee);
+            return new ResponseEntity<>(HttpStatus.OK);
         } else {
             coffeeRepository.save(coffee);
+            return new ResponseEntity<>(HttpStatus.CREATED);
         }
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteCoffee(
             @PathVariable String id
     ) {
